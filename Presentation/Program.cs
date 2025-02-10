8875df3372de635307729560fdf020f8680a28b1
insertokername <iliedemian07@gmail.com>
2025-02-10 15:24:36 +0200

    added https

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,4 +1,5 @@
 using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.RateLimiting;
 using BackendOlimpiadaIsto.application.Commands.GenericCommands;
@@ -14,6 +15,38 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
+var protocol = builder.Configuration["API_PROTOCOL"]?.ToLower() ?? "http";
+if (protocol == "https")
+{
+    var certPemPath = "/etc/letsencrypt/live/bangladesh.go.ro/fullchain.pem";
+    var keyPemPath = "/etc/letsencrypt/live/bangladesh.go.ro/privkey.pem";
+
+    string fullChain = File.ReadAllText(certPemPath);
+    string keyContent = File.ReadAllText(keyPemPath);
+
+    const string certStart = "-----BEGIN CERTIFICATE-----";
+    const string certEnd = "-----END CERTIFICATE-----";
+    int startIndex = fullChain.IndexOf(certStart);
+    int endIndex = fullChain.IndexOf(certEnd);
+
+    if (startIndex < 0 || endIndex < 0)
+    {
+        throw new Exception("Certificate PEM not in expected format.");
+    }
+
+    int length = endIndex - startIndex + certEnd.Length;
+    string leafCert = fullChain.Substring(startIndex, length);
+
+    var x509 = X509Certificate2.CreateFromPem(leafCert, keyContent);
+
+    builder.WebHost.ConfigureKestrel(options =>
+    {
+        options.ListenAnyIP(8888, listenOptions =>
+        {
+            listenOptions.UseHttps(x509);
+        });
+    });
+}
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
