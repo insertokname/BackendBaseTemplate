420f25a7ce6e72eaa555f52b3cbf8a0ff683f866
insertokname <insertokname@gmail.com>
2025-02-13 21:18:05 +0200

    cleaned up verification code

--- Application/src/Commands/Questions/VerifyQuestionHandler.cs
+++ Application/src/Commands/Questions/VerifyQuestionHandler.cs
@@ -31,53 +31,45 @@ public class VerifyQuestionHandler
         var question = await _questionRepository.GetByIdAsync(command.QuestionId);
         if (question == null)
             throw new NotFoundException($"Question not found for the given id: {command.QuestionId}");
+        
         if (!(0 <= command.GivenAnswerIndex && command.GivenAnswerIndex < question.Answers.Answers.Count()))
-        {
             throw new ArgumentException($"Give bad answer index: {command.GivenAnswerIndex}!Out of bounds! (0,{question.Answers.Answers.Count() - 1})");
-        }
+        
+
         bool isCorrect = question.Answers.CorrectAnswerIndex == command.GivenAnswerIndex;
+        if (userId == null || userId == _secretsManager.DefaultAdminGuid)
+            return isCorrect;
+        
+
+        var user = await _userRepository.GetByIdAsync(userId);
+        if (user == null)
+            throw new NotFoundException($"user not found for the given id: {userId}");
 
 
-        if (userId != null && userId != _secretsManager.DefaultAdminGuid)
+        var answeredQuestion = user.AnsweredQuestions
+            .FirstOrDefault(aq => aq.QuestionId == command.QuestionId);
+        if (answeredQuestion == null)
         {
-            var user = await _userRepository.GetByIdAsync(userId);
-            if (user == null)
-            {
-                throw new NotFoundException($"user not found for the given id: {userId}");
-            }
-
-            bool modified = false;
-            var answeredQuestion = user.AnsweredQuestions
-                .FirstOrDefault(aq => aq.QuestionId == command.QuestionId);
-            if (answeredQuestion == null)
-            {
-                answeredQuestion =
-                    new AnsweredQuestion(command.QuestionId, new List<int> { command.GivenAnswerIndex }, isCorrect);
-                user.AnsweredQuestions.Add(answeredQuestion);
-                modified = true;
-            }
-            {
-                if (answeredQuestion.IsFinished)
-                {
-                    throw new ArgumentException("Question already answered corectly!");
-                }
-                    if (!answeredQuestion.Attempts.Any(e => e == command.GivenAnswerIndex))
-                    {
-                        answeredQuestion.Attempts.Add(command.GivenAnswerIndex);
-                        modified = true;
-                    }
-                    if (answeredQuestion.IsFinished != isCorrect)
-                    {
-                        answeredQuestion.IsFinished = isCorrect;
-                        modified = true;
-                    }
-            }
-            if (modified)
-            {
-                _userRepository.Update(user);
-                await _userRepository.SaveChangesAsync();
-            }
+            answeredQuestion =
+                new AnsweredQuestion(command.QuestionId, [], false);
+            user.AnsweredQuestions.Add(answeredQuestion);
         }
+
+        if (answeredQuestion.IsFinished)
+            throw new ArgumentException("Question already answered corectly!");
+
+
+        if (!answeredQuestion.Attempts.Any(e => e == command.GivenAnswerIndex))
+        {
+            answeredQuestion.Attempts.Add(command.GivenAnswerIndex);
+        }
+        if (answeredQuestion.IsFinished != isCorrect)
+        {
+            answeredQuestion.IsFinished = isCorrect;
+        }
+
+        _userRepository.Update(user);
+        await _userRepository.SaveChangesAsync();
         return isCorrect;
     }
 }
